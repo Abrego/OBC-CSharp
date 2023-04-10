93cb48e38274e25985adf7bb49cb48a54cec03ad
Carlos Alberto Abrego Hernández <carlos@abrego.dev>
2023-04-09 23:12:20 -0600

    Terminados ejercicios de la sesión 1

---
+++ Sesion 1/Program.cs
@@ -0,0 +1,9 @@
+﻿string? nombre, hora;
+
+Console.WriteLine("Escribe tu nombre: ");
+nombre = Console.ReadLine();
+Console.WriteLine($"Nombre: {nombre}");
+
+Console.WriteLine("Escribe la hora actual: ");
+hora = Console.ReadLine();
+Console.WriteLine($"Hora actual: {hora}");
